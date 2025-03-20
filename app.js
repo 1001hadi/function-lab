6e2670ad4e5dfc59fd39422f55e29d1bd16efd82
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-03-20 15:20:42 -0600

    sum of array solved

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hey Function");
+const numArr = [6, 37, 10, 36, 1];
+
+function getSum(arr) {
+  let sum = 0;
+  for (let num of arr) {
+    sum += num;
+  }
+  return sum;
+}
+
+console.log(getSum(numArr));
