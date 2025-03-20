4eda4f02d9d3c6d82562246b6961de1ee9389168
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-03-20 15:40:01 -0600

    longest string solved

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const numArr = [6, 37, 10, 36, 1];
+const strArr = [
+  "honda",
+  "toyota",
+  "subaru",
+  "mercedes-benz",
+  "nissan",
+  "mazda",
+];
 
 ///// sum of array -------
 
@@ -14,12 +22,27 @@ const numArr = [6, 37, 10, 36, 1];
 
 ///// average of array ------
 
-function getAverage(arr) {
-  let average = 0;
-  for (let num of arr) {
-    average += num;
+// function getAverage(arr) {
+//   let average = 0;
+//   for (let num of arr) {
+//     average += num;
+//   }
+//   return average / arr.length;
+// }
+
+// console.log(getAverage(numArr));
+
+////// Longest String -------
+
+function getLongestString(arr) {
+  let longestStr = "";
+  for (let i = 0; i < arr.length; i++) {
+    let chars = arr[i];
+    if (chars.length > longestStr.length) {
+      longestStr = chars;
+    }
   }
-  return average / arr.length;
+  return longestStr;
 }
 
-console.log(getAverage(numArr));
+console.log(getLongestString(strArr));
