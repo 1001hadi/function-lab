b71f7e51d01566086b20cb4deea51d2bca6fee07
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-03-20 22:54:30 -0600

    unnecessary comment deleted

--- app.js
+++ app.js
@@ -11,56 +11,56 @@ const strArr = [
 
 ///// sum of array -------
 
-// function getSum(arr) {
-//   let sum = 0;
-//   for (let num of arr) {
-//     sum += num;
-//   }
-//   return sum;
-// }
+function getSum(arr) {
+  let sum = 0;
+  for (let num of arr) {
+    sum += num;
+  }
+  return sum;
+}
 
-// console.log(getSum(numArr));
+console.log(getSum(numArr));
 
 ///// average of array ------
 
-// function getAverage(arr) {
-//   let average = 0;
-//   for (let num of arr) {
-//     average += num;
-//   }
-//   return average / arr.length;
-// }
+function getAverage(arr) {
+  let average = 0;
+  for (let num of arr) {
+    average += num;
+  }
+  return average / arr.length;
+}
 
-// console.log(getAverage(numArr));
+console.log(getAverage(numArr));
 
 ////// Longest String -------
 
-// function getLongestString(arr) {
-//   let longestStr = "";
-//   for (let i = 0; i < arr.length; i++) {
-//     let chars = arr[i];
-//     if (chars.length > longestStr.length) {
-//       longestStr = chars;
-//     }
-//   }
-//   return longestStr;
-// }
+function getLongestString(arr) {
+  let longestStr = "";
+  for (let i = 0; i < arr.length; i++) {
+    let chars = arr[i];
+    if (chars.length > longestStr.length) {
+      longestStr = chars;
+    }
+  }
+  return longestStr;
+}
 
-// console.log(getLongestString(strArr));
+console.log(getLongestString(strArr));
 
 ////// return the string that longer than given number -------
 
-// function getArrOfLongerString(arr, num) {
-//   let longerStr = [];
-//   for (let chars of arr) {
-//     if (chars.length > num) {
-//       longerStr.push(chars);
-//     }
-//   }
-//   return longerStr;
-// }
+function getArrOfLongerString(arr, num) {
+  let longerStr = [];
+  for (let chars of arr) {
+    if (chars.length > num) {
+      longerStr.push(chars);
+    }
+  }
+  return longerStr;
+}
 
-// console.log(getArrOfLongerString(strArr, 6));
+console.log(getArrOfLongerString(strArr, 6));
 
 ////////-------------------------------///////////
 //////// Part 2: Thinking Methodically ///////////
